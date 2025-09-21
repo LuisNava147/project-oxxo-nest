89354449b36c5c33beca4f9c51c603f6c2d26d8e
LuisNava147 <luis.navas147@gmail.com>
2025-09-21 03:53:21 -0600

    New Entities

--- src/locations/dto/create-location.dto.ts
+++ src/locations/dto/create-location.dto.ts
@@ -1,1 +1,14 @@
-export class CreateLocationDto {}
+import { IsString,MaxLength,IsEmail,IsOptional, IsArray, ArrayNotEmpty} from "class-validator";
+import { Location } from "../entities/location.entity";
+
+export class CreateLocationDto extends Location {
+@IsString() 
+@MaxLength(35)
+locationName: string;
+@IsString()
+@MaxLength(130)
+locationAddress: string;
+@IsArray()
+@ArrayNotEmpty()
+locationLatLng: number[];
+}
--- src/locations/entities/location.entity.ts
+++ src/locations/entities/location.entity.ts
@@ -1,1 +1,15 @@
-export class Location {}
+import {Entity, Column, PrimaryGeneratedColumn }from "typeorm";
+@Entity()
+export class Location {
+    @PrimaryGeneratedColumn('increment')
+    locationId: number;
+    @Column('text')
+    locationName: string;
+    @Column('text')
+    locationAddress: string;
+    @Column('double precision',{array:true, nullable: true})
+    locationLatLng: number[];
+
+
+
+}
