b1263c82e78722ee4cd26b3b41a509cf5a328a61
LuisNava147 <luis.navas147@gmail.com>
2025-11-11 00:01:26 -0600

    Fix: main.ts para subir fotos con url y espacio mas grande

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder,SwaggerModule } from '@nestjs/swagger';
 import cookieParser from 'cookie-parser';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{
@@ -11,6 +12,8 @@ async function bootstrap() {
       credentials: true
     }
   });
+  app.use(bodyParser.json({ limit: '10mb' }));
+  app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
   app.use(cookieParser());
   const config = new DocumentBuilder()
     .setTitle('Oxxo API')
